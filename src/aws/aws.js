07f57dff1770b4f8e6b8ccabe384d73e97f7ef95
Prachi Rakhonde <prachirakhonde2000@gmail.com>
2022-09-28 18:59:30 +0530

    AWS Completed

--- src/aws/aws.js
+++ src/aws/aws.js
@@ -1,53 +1,55 @@
-const aws = require("aws-sdk")
-
-
+const aws = require("aws-sdk");
+//Aws Cofiger
 
 
 aws.config.update({
-    accessKeyId: "AKIAY3L35MCRVFM24Q7U",
-    secretAccessKey: "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",
-    region: "ap-south-1"
-})
+    accessKeyId: "AKIAY3L35MCRZNIRGT6N",
+    secretAccessKey: "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",
+    region: "ap-south-1",
+});
 
-let uploadFile = async (file) => {
+//using Upload fike Function
+
+var uploadFile = async (file) => {
     return new Promise(function (resolve, reject) {
-
-        let s3 = new aws.S3({ apiVersion: '2006-03-01' });
+        // this function will upload file to aws and return the link
+        let s3 = new aws.S3({ apiVersion: "2006-03-01" });
 
         var uploadParams = {
             ACL: "public-read",
-            Bucket: "classroom-training-bucket",
-            Key: "group28/" + file.originalname,
-            Body: file.buffer
-        }
+            Bucket: "classroom-training-bucket", //HERE
+            Key: "abc/" + file.originalname, //HERE
+            Body: file.buffer,
+        };
 
         s3.upload(uploadParams, function (err, data) {
             if (err) {
-                return reject({ error: err })
+                return reject({ error: err });
             }
-            console.log(data)
-            console.log("file uploaded sucessfully")
-            return resolve(data.Location)
+            console.log(data);
+            console.log("file uploaded succesfully");
+            return resolve(data.Location);
         })
     })
-
 }
 
-const createLink = async function (req, res) {
-    try {
-        let files = req.files
+createCover = async function (req, res) {
+    try 
+    {
+        let files = req.files;
         if (files && files.length > 0) {
-            let uploadedFileURL = await uploadFile(files[0])
-            res.status(201).send({ msg: "file uploaded sucessfully", data: uploadedFileURL })
+            let uploadedFileURL = await uploadFile(files[0]);
+            res.status(201).send({ msg: "file uploaded succesfully", data: uploadedFileURL });
         }
         else {
-            res.status(400).send({ msg: "no file found" })
+            res.status(400).send({ msg: "No file found" });
         }
-
-    }
+    } 
+    
     catch (err) {
-        res.status(500).send({ msg: err.message })
+        res.status(500).send({ msg: err });
     }
-}
+};
 
-module.exports= {createLink,uploadFile}
+module.exports = {createCover}
+
