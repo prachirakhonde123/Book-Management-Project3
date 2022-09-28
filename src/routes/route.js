2ffd1ea13889f3416cdeaf23d67fac008d634848
Prachi Rakhonde <prachirakhonde2000@gmail.com>
2022-09-28 17:03:51 +0530

    aws Added

--- src/routes/route.js
+++ src/routes/route.js
@@ -4,8 +4,8 @@ const { createUser, userLogin } = require("../controllers/userController")
 const { createBooks, getBooks, getBookById, updateBooks, deleteBooks } = require("../controllers/booksController")
 const { createReview,updateReview,deleteReview } = require('../controllers/reviewController')
 const { authentication, authorisation } = require("../middleware/auth");
-// const aws = require("aws-sdk");
-const { createLink,uploadFile } = require('../aws/aws');
+const aws = require("aws-sdk");
+
 
 
 
@@ -17,54 +17,53 @@ router.post("/login", userLogin)
 //========================= aws link creation ===================================================
 
 
-// aws.config.update({
-//     accessKeyId: "AKIAY3L35MCRZNIRGT6N",
-//     secretAccessKey: "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",
-//     region: "ap-south-1"
-// })
+aws.config.update({
+    accessKeyId: "AKIAY3L35MCRZNIRGT6N",
+    secretAccessKey: "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",
+    region: "ap-south-1"
+})
 
-// let uploadFile = async (file) => {
-//     return new Promise(function (resolve, reject) {
+let uploadFile = async (file) => {
+    return new Promise(function (resolve, reject) {
 
-//         let s3 = new aws.S3({ apiVersion: '2006-03-01' });
+        let s3 = new aws.S3({ apiVersion: '2006-03-01' });
 
-//         var uploadParams = {
-//             ACL: "public-read",
-//             Bucket: "classroom-training-bucket",
-//             Key: "group28/" + file.originalname,
-//             Body: file.buffer
-//         }
+        var uploadParams = {
+            ACL: "public-read",
+            Bucket: "classroom-training-bucket",
+            Key: "group28/" + file.originalname,
+            Body: file.buffer
+        }
 
-//         s3.upload(uploadParams, function (err, data) {
-//             if (err) {
-//                 return reject({ error: err })
-//             }
-//             console.log(data)
-//             console.log("file uploaded sucessfully")
-//             return resolve(data.Location)
-//         })
-//     })
+        s3.upload(uploadParams, function (err, data) {
+            if (err) {
+                return reject({ error: err })
+            }
+            console.log(data)
+            console.log("file uploaded sucessfully")
+            return resolve(data.Location)
+        })
+    })
 
-// }
+}
 
 
-router.post('/aws-file-uploading', createLink
-// async function (req, res) {
-//     try {
-//         let files = req.files
-//         if (files && files.length > 0) {
-//             let uploadedFileURL = await uploadFile(files[0])
-//             res.status(201).send({ msg: "file uploaded sucessfully", data: uploadedFileURL })
-//         }
-//         else {
-//             res.status(400).send({ msg: "no file found" })
-//         }
+router.post('/aws-file-uploading', async function (req, res) {
+    try {
+        let files = req.files
+        if (files && files.length > 0) {
+            let uploadedFileURL = await uploadFile(files[0])
+            res.status(201).send({ msg: "file uploaded sucessfully", data: uploadedFileURL })
+        }
+        else {
+            res.status(400).send({ msg: "no file found" })
+        }
 
-//     }
-//     catch (err) {
-//         res.status(500).send({ msg: err.message })
-//     }
-// }
+    }
+    catch (err) {
+        res.status(500).send({ msg: err.message })
+    }
+}
 )
 
 //========================= book apis =========================================================
